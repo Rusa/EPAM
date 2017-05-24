cab8e5ed0429415cf4e9f1c23be1d886303c6f8a
Rustam <dr.rustam@gmail.com>
2017-05-24 22:37:11 +0300

    javase03 t03 Refactoring

--- src/main/java/javase03/t03/Solution.java
+++ src/main/java/javase03/t03/Solution.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static com.apple.eio.FileManager.getResource;
 
 /**
  * Created by rusamaha on 5/23/17.
@@ -15,12 +14,21 @@ public class Solution {
     public static void main(String[] args) throws FileNotFoundException {
 
         StringBuilder file = readFile("attachment.html");
-        List<String> listOfSentence  = allSentencesWithPic(file);
+        List<String> listOfSPic = allSentencesWithPic(file.toString());
 
-        System.out.println("Предложения в которых стречаются ссылки на рисунки: ");
-        int i =0;
-        for(String s: listOfSentence){
-            System.out.println(++i +") "+ s);
+        System.out.println("Предложения в которых встречаются ссылки на рисунки: ");
+        int i = 0;
+        for (String s : listOfSPic) {
+            System.out.println(++i + ") " + s);
+        }
+
+        System.out.println("Посследовательность ссылок на рисунки: ");
+        boolean isCons = isPicConsistently(file.toString());
+
+        if (isCons) {
+            System.out.println("\nНомера упорядоченны");
+        } else {
+            System.out.println("\nНомера не упорядоченны");
         }
     }
 
@@ -36,48 +44,50 @@ public class Solution {
         return sb;
     }
 
-    private boolean isPicConsistently(String s){
-        Pattern pattern = Pattern.compile("[<img]?>((((Рис\\.)\\s*)(\\d+)).*?)"); // only text with number under pictures
+    /**
+     * @param str String to search
+     * @return <code>true</code> if nums of pictures ordered
+     */
+    private static boolean isPicConsistently(String str) {
 
+        Pattern patternImg = Pattern.compile("[<img]?>((((Рис\\.)\\s*)(\\d+)).*?)");   // only text with number under pictures for solving over conditions
+        Pattern patternImgN = Pattern.compile("(р|Р)ис\\.?\\s*\\D{0,5}\\s*(\\d+)");  // number after picture word
+
+        Matcher matcher;
+
+        ArrayList<Integer> orderList = new ArrayList<>();
+        for (String s : allSentencesWithPic(str)) {
+            matcher = patternImgN.matcher(s);
+            while (matcher.find()) {
+                int numOfPic = Integer.parseInt(matcher.group(2));
+                orderList.add(numOfPic);
+                System.out.print(numOfPic + " ");
+            }
+        }
+        int current = 0;
+
+        for (Integer i : orderList) {
+            if(i<current) return false;
+            current = i;
+        }
         return true;
     }
 
 
     /**
-     *  Whole sentences with "рис NUM"(and variations) without text under images
      *
      * @param file
-     * @return
+     * @return All sentences with "рис NUM"(and variations) without Sentences under images
      */
-    private static List<String> allSentencesWithPic(StringBuilder file){
+    private static List<String> allSentencesWithPic(String file) {
         List<String> list = new ArrayList<>();
-//        Pattern pattern = Pattern.compile("([А-Я][^.?!]+?)(((((р|Р)ис\\.?)\\s*\\D{0,5}\\s*)(\\d+)).*?)+?[.!?]\\s*?(?![А-Я])");
         Pattern pattern = Pattern.compile("([А-Я][^.?!]+?)(((((р|Р)ис\\.?)\\s*\\D{0,5}\\s*)(\\d+)).*?)+?[.!?]\\s*?");
         Matcher matcher = pattern.matcher(file);
         int i = 0;
-        while (matcher.find()){
+        while (matcher.find()) {
             list.add(matcher.group(0));
-
         }
-
         return list;
-
-//        рис\.?\s*\d{0,3}
-
-
-//        ((р|Р)ис\.?\s*\D?)(\d+)
-//        ((р|Р)ис\.+\s*\D*)(\d+)
-
-
-//        ((р|Р)ис\.?\s*\D{0,5}\s*)(\d+)\s*
-//        ((р|Р)ис\.?\s*\D{0,5}\s*)(\d+)\s*(\d?)
-//        ((р|Р)ис\.?\s*\D{0,5}\s*)(\d+)
-//        ((р|Р)ис\.?\s*\D{0,5}\s*)(\d+)
-//        (?>([А-Я].*?)(рис.*?)(\.))
-
-//        ([А-Я][^\.\?\!]+?)(((((р|Р)ис\.?)\s*\D{0,5}\s*)(\d+)).*?)+?[\.\!\?]\s*?(?![А-Я])
-
-
     }
 
     private static InputStreamReader getResource(String name) throws UnsupportedEncodingException {
